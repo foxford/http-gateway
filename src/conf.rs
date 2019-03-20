9280c4c49b90c6232ad030adee0c48b33165047e
Andrei Nesterov <ae.nesterov@gmail.com>
2019-03-20 10:10:26 -0700

    Replace Deployment with StatefulSet

--- src/conf.rs
+++ src/conf.rs
@@ -1,16 +1,17 @@
 use config;
 
 #[derive(Debug, Deserialize)]
-pub struct Config {
-    pub id: svc_authn::AccountId,
-    pub authn: svc_authn::jose::ConfigMap,
-    pub web: crate::web::Config,
-    pub mqtt: svc_agent::mqtt::AgentConfig,
+pub(crate) struct Config {
+    pub(crate) id: svc_authn::AccountId,
+    pub(crate) agent_label: String,
+    pub(crate) authn: svc_authn::jose::ConfigMap,
+    pub(crate) web: crate::web::Config,
+    pub(crate) mqtt: svc_agent::mqtt::AgentConfig,
     #[serde(default)]
-    pub events: crate::event::ConfigMap,
+    pub(crate) events: crate::event::ConfigMap,
 }
 
-pub fn load() -> Result<Config, config::ConfigError> {
+pub(crate) fn load() -> Result<Config, config::ConfigError> {
     let mut parser = config::Config::default();
     parser.merge(config::File::with_name("App"))?;
     parser.merge(config::Environment::with_prefix("APP").separator("__"))?;
