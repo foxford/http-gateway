e25c8f09ec15fb5ed4f3710f0807f6381b9cad6e
Timofey Martynov <feymartynov@gmail.com>
2020-02-05 06:35:44 +0300

    Proxy MQTT properties to HTTP headers

---
+++ src/util/headers.rs
@@ -0,0 +1,56 @@
+use std::convert::TryFrom;
+
+use failure::{err_msg, format_err, Error};
+use http::header::{HeaderMap, HeaderName, HeaderValue};
+use serde::ser::Serialize;
+use svc_agent::{mqtt::IncomingMessage, Addressable};
+
+#[derive(Debug)]
+pub(crate) struct Headers(Vec<(String, String)>);
+
+impl Headers {
+    pub(crate) fn add_to_header_map(&self, header_map: &mut HeaderMap) {
+        header_map.reserve(self.0.len());
+
+        for (name_str, value_str) in self.0.iter() {
+            let prefixed_name = format!("gateway-{}", name_str.replace("_", "-"));
+
+            if let Ok(name) = HeaderName::from_bytes(prefixed_name.as_bytes()) {
+                if let Ok(value) = HeaderValue::from_bytes(value_str.as_bytes()) {
+                    header_map.append(name, value);
+                }
+            }
+        }
+    }
+
+    pub(crate) fn to_header_map(&self) -> HeaderMap {
+        let mut header_map = HeaderMap::new();
+        self.add_to_header_map(&mut header_map);
+        header_map
+    }
+}
+
+impl<T, P: Addressable + Serialize> TryFrom<&IncomingMessage<T, P>> for Headers {
+    type Error = Error;
+
+    fn try_from(message: &IncomingMessage<T, P>) -> Result<Self, Self::Error> {
+        let props_value = serde_json::to_value(message.properties())
+            .map_err(|err| format_err!("Failed to serialize properties: {}", err))?;
+
+        let props_object = props_value
+            .as_object()
+            .ok_or_else(|| err_msg("Properties is not an object"))?;
+
+        let mut headers = Vec::with_capacity(props_object.len());
+
+        for (key, json_value) in props_object.into_iter() {
+            let value = json_value
+                .as_str()
+                .ok_or_else(|| format_err!("Property '{}' has not a string value", key))?;
+
+            headers.push((key.to_owned(), value.to_string()));
+        }
+
+        Ok(Self(headers))
+    }
+}
